1ca0e85c46c77bcb755af02a0ca4a0ea0441f23f
Maxence <lemairemaxence@hotmail.com>
2021-01-12 16:41:11 +0100

    exo 9 à finir

---
+++ public/js/main.js
@@ -0,0 +1,94 @@
+// EXO 1
+
+// let fonction = (a,b) => {
+//     return (a + b)
+// }
+
+// let addition = fonction(1, 2)
+// console.log(addition);
+
+// EXO 2
+
+// let fonction = (a,b) => {
+//     return (b - a)
+// }
+
+// let soustraction = fonction(2, 8)
+// console.log(soustraction);
+
+// EXO 3
+
+// let fonction = (a,b) => {
+//     return (a * b)
+// }
+
+// let multiplication = fonction(2, 5)
+// console.log(multiplication);
+
+// EXO 4
+
+// let fonction = (a,b) => {
+//     return (a / b)
+// }
+
+// let division = fonction(6, 3)
+// console.log(division);
+
+// EXO 5
+
+// let fonction = (a,b) => {
+//     return (a % b)
+// }
+
+// let rest = fonction(12, 5)
+// console.log(rest);
+
+// EXO 6
+
+// let fonction = (a) => {
+//     return Math.sqrt(a)
+// }
+
+// let squareRoot = fonction(9)
+// console.log(squareRoot);
+
+// EXO 7
+
+// let fonction = (a,b) => {
+//     return Math.pow(a,b)
+// }
+
+// let exposant = fonction(3,2)
+// console.log(exposant);
+
+// EXO 8
+
+// let string = prompt("Phrase :")
+
+// let capitalize = () => {
+//     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+// }
+
+// console.log(capitalize());
+
+// EXO 9
+
+let nbr = parseInt(prompt = ("Nombre 1"))
+let nbr2 = parseInt(prompt = ("Nombre 2"));
+
+let operator;
+
+switch (operator) {
+    case "+":
+        
+        break;
+
+    default:
+        break;
+}
+
+let calcul = (nbr, operator, nbr2) => {
+    (nbr operator nbr2)
+}
+
+console.log(calcul());
