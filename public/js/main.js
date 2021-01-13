d4ce0bc62d4c23c9154561176c07b538bcf45a41
Maxence <lemairemaxence@hotmail.com>
2021-01-13 14:39:46 +0100

    exo corrigé

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,49 @@
 // EXO 1
 
-// let fonction = (a,b) => {
-//     return (a + b)
+// let add = (a,b) => {
+//     return `${a} + ${b} = ${a + b}`
 // }
 
-// let addition = fonction(1, 2)
-// console.log(addition);
+// console.log(add(1, 2));
 
 // EXO 2
 
-// let fonction = (a,b) => {
-//     return (b - a)
+// let sous = (a,b) => {
+//     return `${b} - ${a} = ${b - a}`
 // }
 
-// let soustraction = fonction(2, 8)
-// console.log(soustraction);
+// console.log(sous(6, 8));
+
 
 // EXO 3
 
-// let fonction = (a,b) => {
-//     return (a * b)
+// let multi = (a,b) => {
+//     return `${a} * ${b} = ${a * b}`
 // }
 
-// let multiplication = fonction(2, 5)
-// console.log(multiplication);
+// console.log(multi(5, 2));
 
 // EXO 4
 
-// let fonction = (a,b) => {
-//     return (a / b)
+// let div = (a,b) => {
+//     return `${a} / ${b} = ${a / b}`
 // }
 
-// let division = fonction(6, 3)
-// console.log(division);
+// console.log(div(10, 5));
+
 
 // EXO 5
 
-// let fonction = (a,b) => {
-//     return (a % b)
+// let reste = (a,b) => {
+//     return `${a} % ${b} = ${a % b}`
 // }
 
-// let rest = fonction(12, 5)
-// console.log(rest);
+// console.log(reste(12, 5));
 
-// EXO 6
+
+// EXO 
+
+// RACINE CARREE
 
 // let fonction = (a) => {
 //     return Math.sqrt(a)
@@ -51,6 +51,14 @@
 
 // let squareRoot = fonction(9)
 // console.log(squareRoot);
+
+// CARRE
+
+// let carre = (a) => {
+//     return (a * a)
+// }
+
+// console.log(carre(2));
 
 // EXO 7
 
@@ -73,50 +81,65 @@
 
 // EXO 9
 
-let nb1 = parseInt(prompt("Nombre 1 ?"));
-let nb2 = parseInt(prompt("Nombre 2 ?"));
-let op = prompt("Opérateur ?");
+// let nb1 = parseInt(prompt("Nombre 1 ?"));
+// let nb2 = parseInt(prompt("Nombre 2 ?"));
+// let op = prompt("Opérateur ?");
 
-let add = (nb1, nb2) => {
-    return (nb1 + nb2);
-}
-let sous = (nb1, nb2) => {
-    return (nb1 - nb2);
-}
-let multi = (nb1, nb2) => {
-    return (nb1 * nb2);
-}
-let div = (nb1, nb2) => {
-    return (nb1 / nb2);
-}
+// let add = (nb1, nb2) => {
+//     return (nb1 + nb2);
+// }
+// let sous = (nb1, nb2) => {
+//     return (nb1 - nb2);
+// }
+// let multi = (nb1, nb2) => {
+//     return (nb1 * nb2);
+// }
+// let div = (nb1, nb2) => {
+//     return (nb1 / nb2);
+// }
 
-let calcul = (nb1, nb2, op) => {
-    let operateurs = ["+", "-", "*", "/"];
-    let resultat;
-    if (operateurs.includes(op)) {
-        switch (op) {
-            case "+":
-                resultat = add(nb1, nb2);
-                break;
-        
-            case "-":
-                resultat = sous(nb1, nb2);
-                break;
-        
-            case "*":
-                resultat = multi(nb1, nb2);
-                break;
-        
-            case "/":
-                resultat = div(nb1, nb2);
-                break;
-        
-            default:
-                console.log("Erreur - mauvais opérateur");
-                break;
-        }
+// let calcul = (nb1, nb2, op) => {
+//     let operateurs = ["+", "-", "*", "/"];
+//     let resultat;
+//     if (operateurs.includes(op)) {
+//         switch (op) {
+//             case "+":
+//                 resultat = add(nb1, nb2);
+//                 break;
+//             case "-":
+//                 resultat = sous(nb1, nb2);
+//                 break;
+//             case "*":
+//                 resultat = multi(nb1, nb2);
+//                 break;
+//             case "/":
+//                 resultat = div(nb1, nb2);
+//                 break;
+//             default:
+//                 console.log("Erreur - mauvais opérateur");
+//                 break;
+//         }
+//     }
+//     return resultat
+// }
+
+// console.log(calcul(nb1, nb2, op));
+
+// METHODE + COURTE
+
+let calcul = (a, b, c) => {
+    switch (b) {
+        case "+":
+            return `${a} + ${c} = ${a+c}`
+        case "-":
+            return `${a} - ${c} = ${a-c}`
+        case "*":
+            return `${a} * ${c} = ${a*c}`
+        case "/":
+            return `${a} / ${c} = ${a/c}`
+        default:
+            return `ERROR -> Not operateur : ${b}`
     }
-    return resultat
 }
 
-console.log(calcul(nb1, nb2, op));
+console.log(calcul(6, "+", 2));
